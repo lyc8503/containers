c4bf9ea3f5431cfcfa88664d4dc483e41d43ca5c
lyc8503 <me@lyc8503.site>
2024-06-12 16:48:08 +0800

    add date folder

--- upload_server/main.py
+++ upload_server/main.py
@@ -1,11 +1,18 @@
+import os
 from fastapi import FastAPI, UploadFile, File
 from pathlib import Path
+from datetime import datetime
 
 app = FastAPI()
 
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
-    file_location = Path("/uploaded") / Path(file.filename).name
+    today = datetime.now().strftime('%Y%m%d')
+    folder_path = Path(f"/uploaded/{today}")
+    if not folder_path.exists():
+        os.mkdir(folder_path)
+
+    file_location = folder_path / Path(file.filename).name
     with open(file_location, "wb+") as file_object:
         file_object.write(file.file.read())
     return {"msg": f"file '{file.filename}' saved"}
