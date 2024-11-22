74ed6a667ba582c856282adfc7938d4a129355a3
lyc8503 <me@lyc8503.net>
2024-11-23 02:25:43 +0800

    single thread gets 200qps, db todo

--- bilibili_full/main.py
+++ bilibili_full/main.py
@@ -1,19 +1,23 @@
 from req import fetch_metadata
 import asyncio
+import tqdm
 
+
+success = 0
 
 async def main():
     tasks = []
-    count = 0
-    for i in range(1, 10_0000_0000):
+    for i in (pbar := tqdm.trange(1, 10_0000_0000)):
         tasks.append(asyncio.create_task(fetch_metadata(i)))
-        while len(tasks) >= 100:
+        while len(tasks) >= 20:
             finished, unfinished = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
             
             for r in finished:
-                count += 1
-                print(count)
-                print(r.result()['code'])
+                if r.result()['code'] == 0:
+                    global success
+                    success += 1
+                    if success % 100 == 0:
+                        pbar.set_description(f"Success: {success}")
             
             tasks = list(unfinished)
 
--- bilibili_full/req.py
+++ bilibili_full/req.py
@@ -1,12 +1,18 @@
-from aiohttp_requests import requests
+import aiohttp
+
+
+HTTP_RPOXY = 'http://127.0.0.1:2345'
 
 async def fetch_metadata(av):
-    req = await requests.get(f"https://api.bilibili.com/x/web-interface/view?aid={av}", headers={
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299",
+    url = f"https://api.bilibili.com/x/web-interface/view?aid={av}"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
         "Referer": f"https://www.bilibili.com/video/av{av}"
-    })
+    }
 
-    try:
-        return await req.json()
-    except:
-        raise Exception(req.text)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers, proxy=HTTP_RPOXY) as response:
+            try:
+                return await response.json()
+            except:
+                raise Exception(await response.text())
