39823c9892bc2e8dbbd679b7329a933adc7e8b41
lyc8503 <me@lyc8503.net>
2024-11-24 20:50:13 +0800

    Mongo and 1280 QPS!

--- bilibili_full/main.py
+++ bilibili_full/main.py
@@ -1,27 +1,35 @@
-from req import fetch_metadata
+import aiohttp
 import asyncio
+from req import fetch_metadata
 import tqdm
+from pymongo import AsyncMongoClient
 
 
-success = 0
+stats = dict()
+buffer = []
 
 async def main():
-    tasks = []
-    for i in (pbar := tqdm.trange(1, 10_0000_0000)):
-        tasks.append(asyncio.create_task(fetch_metadata(i)))
-        while len(tasks) >= 20:
-            finished, unfinished = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
-            
-            for r in finished:
-                if r.result()['code'] == 0:
-                    global success
-                    success += 1
-                    if success % 100 == 0:
-                        pbar.set_description(f"Success: {success}")
-            
-            tasks = list(unfinished)
+    tasks = set()
+    async with aiohttp.ClientSession() as session, AsyncMongoClient("mongodb://localhost:27017/bili") as client:
+        for i in (pbar := tqdm.trange(5960000, 10_0000_0000)):
+            tasks.add(asyncio.create_task(fetch_metadata(session, i)))
+            while len(tasks) >= 80:
+                finished, tasks = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+                
+                for r in finished:
+                    av, ret = r.result()
+                    
+                    ret['_id'] = av
+                    buffer.append(ret)
+                    stats[ret['code']] = stats.get(ret['code'], 0) + 1
 
+                    if len(buffer) >= 1000:
+                        try:
+                            await client.bili.meta.insert_many(buffer)
+                        except Exception as e:
+                            print(f"Mongo Error: {e}")
+                        print(stats)
+                        buffer.clear()
 
 if __name__ == "__main__":
-    import asyncio
     asyncio.run(main())
--- bilibili_full/req.py
+++ bilibili_full/req.py
@@ -3,16 +3,19 @@ import aiohttp
 
 HTTP_RPOXY = 'http://127.0.0.1:2345'
 
-async def fetch_metadata(av):
+async def fetch_metadata(session, av):
     url = f"https://api.bilibili.com/x/web-interface/view?aid={av}"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
         "Referer": f"https://www.bilibili.com/video/av{av}"
     }
 
-    async with aiohttp.ClientSession() as session:
+    try:
         async with session.get(url, headers=headers, proxy=HTTP_RPOXY) as response:
-            try:
-                return await response.json()
-            except:
-                raise Exception(await response.text())
+            return av, await response.json()
+    except Exception as e:
+        print(f"Error: {e}")
+        return av, {
+            "code": -114514,
+            "err": str(e)
+        }
