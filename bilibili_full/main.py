1e3dd4f30f5a73bd0f20d244014d9c597b6a77e5
lyc8503 <me@lyc8503.net>
2024-11-24 21:51:15 +0800

    multiprocessing, easily reach 10000 QPS! (don't do that)

--- bilibili_full/main.py
+++ bilibili_full/main.py
@@ -3,19 +3,19 @@ import asyncio
 from req import fetch_metadata
 import tqdm
 from pymongo import AsyncMongoClient
-
+from multiprocessing import Process
 
 stats = dict()
 buffer = []
 
-async def main():
+async def main(start, end, pos):
     tasks = set()
     async with aiohttp.ClientSession() as session, AsyncMongoClient("mongodb://localhost:27017/bili") as client:
-        for i in (pbar := tqdm.trange(5960000, 10_0000_0000)):
+        for i in tqdm.trange(start, end, position=pos, desc=f"Process #{pos}"):
             tasks.add(asyncio.create_task(fetch_metadata(session, i)))
-            while len(tasks) >= 80:
+            while len(tasks) >= 80 or (i == end - 1 and tasks):
                 finished, tasks = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
-                
+
                 for r in finished:
                     av, ret = r.result()
                     
@@ -30,6 +30,22 @@ async def main():
                             print(f"Mongo Error: {e}")
                         print(stats)
                         buffer.clear()
+        if buffer:
+            await client.bili.meta.insert_many(buffer)
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    PROCESS_COUNT = 2
+    processes = []
+    start = 0
+    end = 10_0000_0000
+
+    for i in range(PROCESS_COUNT):
+        left = start + (end - start) // PROCESS_COUNT * i
+        right = start + (end - start) // PROCESS_COUNT * (i + 1)
+        p = Process(target=asyncio.run, args=(main(left, right, i),))
+        p.start()
+        processes.append(p)
+    
+    for p in processes:
+        p.join()
