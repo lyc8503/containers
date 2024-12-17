d03b0db7a9e3c485abd65b6330cdee2b65ffc005
lyc8503 <me@lyc8503.net>
2024-12-17 17:30:55 +0800

    WIP: lmdb

--- lofter_download/cli_pack_old.py
+++ lofter_download/cli_pack_old.py
@@ -1,12 +1,11 @@
-# this is a utility script to pack the old downloaded files into a berkeley db
+# this is a utility script to pack the old downloaded files into an lmdb database
 # run it manually
 
 import pickle
-from berkeleydb import db as bdb
+import lmdb
 import json
 import os
 import time
-
 
 class SetEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -14,39 +13,50 @@ class SetEncoder(json.JSONEncoder):
             return list(obj)
         return json.JSONEncoder.default(self, obj)
 
-
-if __name__ == "__main__":
-    if os.path.exists("/download/posts.db"):
-        print("file exists")
-        exit(1)
-
-    db = bdb.DB()
-    db.open("/download/posts.db", None, bdb.DB_HASH, bdb.DB_CREATE)
+def pack_data(env):
+    counter = 0
+    t = time.time()
+    txn = env.begin(write=True)
 
     for root, dirs, files in os.walk("/download/"):
         for file in files:
-            if '.pickle' not in file:  # date: folder
+            if '.pickle' not in file:
                 continue
-            
+
             p = os.path.join(root, file)
             with open(p, "rb") as f:
                 key = file.replace('.pickle', '') + "_" + root.split('/')[-1]
                 print(f"packing {key}")
                 content = pickle.load(f)
                 content = json.dumps(content, ensure_ascii=False, cls=SetEncoder).encode("utf-8")
-                db.put(key.encode('utf-8'), content)
-    
-    counter = 0
-    t = time.time()
+                txn.put(key.encode('utf-8'), content)
+                counter += 1
+
+                if counter % 1000 == 0:
+                    txn.commit()
+                    txn = env.begin(write=True)
 
     for root, dirs, files in os.walk("/download/posts"):
         for file in files:
             with open(os.path.join(root, file), "rb") as f:
                 data = json.load(f)
-                db.put(file.replace('.json', '').encode('utf-8'), json.dumps(data, ensure_ascii=False).encode("utf-8"))
+                txn.put(file.replace('.json', '').encode('utf-8'), json.dumps(data, ensure_ascii=False).encode("utf-8"))
                 counter += 1
                 if counter % 1000 == 0:
                     print(f"{counter} files packed, {1000 / (time.time() - t):.1f} op/s, current: {os.path.join(root, file)}")
                     t = time.time()
+                    if counter % 10000 == 0:
+                        txn.commit()
+                        print("txn committed")
+                        txn = env.begin(write=True)
 
-    db.close()
+    txn.commit()
+
+if __name__ == "__main__":
+    if os.path.exists("/download/posts.db"):
+        print("file exists")
+        exit(1)
+
+    env = lmdb.open('/download/posts.db', map_size=1099511627776)
+    pack_data(env)
+    env.close()
--- lofter_download/cli_search.py
+++ lofter_download/cli_search.py
@@ -1,38 +1,31 @@
 import json
-from berkeleydb import db as bdb
+import lmdb
 import time
 from tqdm import tqdm
 
-
-db = bdb.DB()
-db.open("/download/posts.db", None, bdb.DB_HASH)
-
+env = lmdb.open("/download/posts.db", readonly=True)
 keyword = input("keyword: ")
 print()
 
 start = time.time()
 counter = 0
 
-pbar = tqdm()
-cursor = db.cursor()
-record = cursor.first()
-
+pbar = tqdm(total=env.stat()['entries'])
 stat = dict()
 
-while record:
-    pbar.update(1)
-    k, v = record
-    k, v = k.decode('utf-8'), v.decode('utf-8')
+with env.begin() as txn:
+    cursor = txn.cursor()
+    for key, value in cursor:
+        pbar.update(1)
+        k, v = key.decode('utf-8'), value.decode('utf-8')
 
-    if k.startswith("stat"):
-        stat[k] = json.loads(v)
-        record = cursor.next()
-        continue
-    
-    if keyword in v:
-        print(v)
-        counter += 1
-    record = cursor.next()
+        if k.startswith("stat"):
+            stat[k] = json.loads(v)
+            continue
+
+        if keyword in v:
+            print(v)
+            counter += 1
 
 print(f"Found {counter} posts in {time.time() - start:.2f} seconds.")
 print("===== Stats ======")
